317b953d3493d3848f2dd1c3c275d64a71e38201
Thomas Nguyen <thomas_ejob@hotmail.com>
2016-12-15 02:15:13 -0800

    setup to run TestNG

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,6 +2,7 @@ package runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 import org.eclipse.jetty.util.security.CertificateUtils;
 import org.junit.runner.RunWith;
 
@@ -9,8 +10,16 @@ import org.junit.runner.RunWith;
  * Created by Thomas on 12/15/2016.
  */
 
-// To run the test from the command lines
+// For running the JUnit tests from the command line
+/*
 @RunWith(Cucumber.class)
-@CucumberOptions(features ={"src/test/java/features"}, glue = {"step_definitions"})
+@CucumberOptions(features ={"src/test/java/features"}, glue = "step_definitions")
 public class TestRunner {
+}
+*/
+
+// For running the TestNg tests from the command line
+@CucumberOptions(features ={"src/test/java/features"}, glue = "step_definitions")
+public class TestRunner extends AbstractTestNGCucumberTests {
+
 }
